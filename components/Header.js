5a188bf625b415e9a4baa7c5652a929e9571dcf1
Daniel Eta <etadaniel7@gmail.com>
2022-02-20 22:29:11 +0200

    first commit

---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+
+const linkStyles = {
+    color: "black",
+    textDecoration: "none",
+    cursor: "pointer",
+};
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <nav className="navigation">
+                    <ul>
+                        <li>
+                            <Link href='/'>
+                                <p style={ linkStyles }>Home</p>
+                            </Link>
+                        </li>
+
+                        <li>-</li>
+
+                        <li>
+                            <Link style={ linkStyles }  href='/'>
+                                <p style={ linkStyles }>Writing</p>
+                            </Link>
+                        </li>
+
+                        <li>-</li>
+
+                        <li>
+                            <Link style={ linkStyles }  href='/'>
+                                <p style={ linkStyles }>Projects</p>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
---
+++ posts/nietzche-monsters.md
@@ -0,0 +1,6 @@
+---
+title: "Nietzche and the regret monsters"
+date: 'February 20 2022'
+excerpt: "This is the excerpt"
+cover_image: '/images/posts/image-1.jpg'
+---
